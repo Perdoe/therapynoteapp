a57d594ab89365806c1960731e2cb56539c0af87
Perdoe <felipe.jalil101@gmail.com>
2025-05-04 15:30:45 -0400

    GitPages Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/therapynoteapp' : '',
+  images: {
+    unoptimized: true,
+  },
+  // This is optional but helpful if you have any TS errors that would block the build
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
